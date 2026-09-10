dea8bb620072b2df1f3abd58dceb08c64f2f11fd
shalure10lure <yessicarado@gmail.com>
2026-09-09 22:56:44 -0400

    feat: conectar presenter con totalizador de venta

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,22 @@
-import sumar from "./sumador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
+import Ventas from "./ventas.js";
+const cantidad = document.querySelector("#cantidad");
+const precio = document.querySelector("#precio");
+const estado = document.querySelector("#estado");
+const form = document.querySelector("#ventas-form");
 const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
-  event.preventDefault();
+    event.preventDefault();
+    const cantidadIngresada = Number(cantidad.value);
+    const precioIngresado = Number(precio.value);
+    const estadoIngresado = estado.value.toUpperCase();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+    const ventas = new Ventas();
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+    const precioNeto = ventas.calcularPrecioNeto(
+        cantidadIngresada,
+        precioIngresado
+    );
+
+    div.innerHTML = "<p>Precio neto: $" + precioNeto + "</p>";
 });
